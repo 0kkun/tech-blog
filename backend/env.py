77c755e5b1ace05ab0de0d5f53b529ec1e3d7d2e
0kkun <shinji.okumoto.01@gmail.com>
2023-07-04 18:15:45 +0900

    add: envにdb関係の情報追加

--- backend/env.py
+++ backend/env.py
@@ -7,7 +7,14 @@ def _getenv(key: str, default: str = ""):
         return default
     return env
 
+
 class Env:
     APP_ENV = _getenv("APP_ENV", "local")
     LOG_LEVEL = _getenv("LOG_LEVEL", "debug")
     APP_URL = _getenv("APP_URL")
+    MYSQL_DB_HOST = _getenv("MYSQL_DB_HOST")
+    MYSQL_DB_NAME = _getenv("MYSQL_DB_NAME")
+    MYSQL_HOST_PORT = _getenv("MYSQL_HOST_PORT")
+    MYSQL_USER_NAME = _getenv("MYSQL_USER_NAME")
+    MYSQL_PASSWORD = _getenv("MYSQL_PASSWORD")
+    MYSQL_ROOT_PASSWORD = _getenv("MYSQL_ROOT_PASSWORD")
