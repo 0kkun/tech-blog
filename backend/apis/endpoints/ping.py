874e55b6d3336eafd51b926f68177e76df4ab483
0kkun <shinji.okumoto.01@gmail.com>
2023-07-04 18:15:16 +0900

    fix: フォーマッター実行

--- backend/apis/endpoints/ping.py
+++ backend/apis/endpoints/ping.py
@@ -2,15 +2,17 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 import logging
 
-
 router = APIRouter()
 _logger = logging.getLogger(__name__)
+
 
 class Ping(BaseModel):
     message: str
 
+
 class PingResponse(BaseModel):
     data: Ping
+
 
 @router.get("/ping", summary="アプリからの疎通確認用", tags=["debug"])
 async def ping() -> PingResponse:
