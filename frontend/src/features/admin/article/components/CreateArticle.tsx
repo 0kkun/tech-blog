8227aeb4ed8da5341709b64e9e364b27848b3a63
0kkun <shinji.okumoto.01@gmail.com>
2023-10-19 12:23:57 +0900

    add: コードスタイル修正

--- frontend/src/features/admin/article/components/CreateArticle.tsx
+++ frontend/src/features/admin/article/components/CreateArticle.tsx
@@ -8,6 +8,8 @@ import TextareaAutosize from '@mui/material/TextareaAutosize'
 import { useForm } from 'react-hook-form'
 import ReactMarkdown from 'react-markdown'
 import remarkGfm from 'remark-gfm'
+import {Prism as SyntaxHighlighter} from 'react-syntax-highlighter'
+import { dracula } from 'react-syntax-highlighter/dist/esm/styles/prism'
 
 import Title from '../../../../components/admin/elements/Title'
 
@@ -82,8 +84,42 @@ export const CreateArticle: React.FC = () => {
             >
               <Title>プレビュー画面</Title>
               <div className='markdown-container'>
-                <ReactMarkdown remarkPlugins={[remarkGfm]}>
-                  {inputText}
+                <ReactMarkdown
+                  remarkPlugins={[remarkGfm]}
+                  components={{
+                    h1: "h2",
+                    h2: "h3",
+                    h3: "h4",
+                    h4: "h5",
+                    h5: "h6",
+                    h6: "h6",
+                    code: ({
+                      node,
+                      inline,
+                      className,
+                      style,
+                      children,
+                      ...props
+                    }) => {
+                      const match = /language-(\w+)/.exec(className || "");
+                      return !inline && match ? (
+                        <SyntaxHighlighter
+                          style={dracula}
+                          language={match[1]}
+                          PreTag="div"
+                          {...props}
+                        >
+                          {String(children).replace(/\n$/, "")}
+                        </SyntaxHighlighter>
+                      ) : (
+                        <code className={className} {...props}>
+                          {children}
+                        </code>
+                      );
+                    },
+                  }}
+                  >
+                    {inputText}
                 </ReactMarkdown>
               </div>
             </Paper>
