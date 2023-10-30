fcd3160181cede203c85e2776ef001cd1bf3aaff
0kkun <shinji.okumoto.01@gmail.com>
2023-10-30 19:21:25 +0900

    fix: pmamの表示やめる

--- frontend/src/libs/date.ts
+++ frontend/src/libs/date.ts
@@ -6,9 +6,8 @@ export const formatDateTime = (dateTimeStr: string) => {
   const day = dateTime.getDate()
   const hour = dateTime.getHours()
   const minute = dateTime.getMinutes()
-  const amPm = hour >= 12 ? 'PM' : 'AM'
 
-  const formattedDateTime = `${year}/${month}/${day} ${amPm}${hour
+  const formattedDateTime = `${year}/${month}/${day} ${hour
     .toString()
     .padStart(2, '0')}:${minute.toString().padStart(2, '0')}`
 
