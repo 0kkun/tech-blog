0fcd08d525cee86b35c90f7f33f314745cabc876
0kkun <shinji.okumoto.01@gmail.com>
2023-10-31 09:26:37 +0900

    fix: DEBUG_MODE=trueの時だけログを出すように修正

--- frontend/src/libs/logger.ts
+++ frontend/src/libs/logger.ts
@@ -1,21 +1,18 @@
-
-
-
-
 export const logger = (title: string, message: string, ...subMessages: any[]) => {
-  const black = '\u001b[30m';
-  const red = '\u001b[31m';
-  const green = '\u001b[32m';
-  const yellow = '\u001b[33m';
+  const black = '\u001b[30m'
+  const red = '\u001b[31m'
+  const green = '\u001b[32m'
+  const yellow = '\u001b[33m'
   const blue = '\u001b[34m'
 
-  if (title === 'interceptor') {
+  if (import.meta.env.VITE_DEBUG_MODE == 'true') {
     const titleText = '[' + title + '] '
-    console.log(green + titleText + message, ...subMessages)
-  } else if (title === 'guard') {
-    const titleText = '[' + title + '] '
-    console.log(yellow + titleText + message, ...subMessages)
-  } else {
-    console.log(message, ...subMessages)
+    if (title === 'interceptor') {
+      console.log(green + titleText + message, ...subMessages)
+    } else if (title === 'guard') {
+      console.log(yellow + titleText + message, ...subMessages)
+    } else {
+      console.log(black + titleText + message, ...subMessages)
+    }
   }
 }
