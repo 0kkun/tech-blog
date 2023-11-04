7e991ec3146929f071b4304b7f2284de8528add5
0kkun <shinji.okumoto.01@gmail.com>
2023-11-04 17:04:42 +0900

    clear: articleリファクタ2

--- frontend/src/pages/admin/AdminDraftIndexArticle.tsx
+++ frontend/src/pages/admin/AdminDraftIndexArticle.tsx
@@ -1,12 +1,54 @@
-import { FC } from 'react'
+import React, { useState, useEffect } from 'react'
 import Grid from '@mui/material/Grid'
 import { AdminTemplate } from '../../components/admin/templates/AdminTemplate'
 import { ArticleTable } from '../../features/admin/article/components/ArticleTable'
+import { useFetchArticles } from '../../features/admin/article/hooks/useFetchArticles'
+import { useDeleteArticle } from '../../features/admin/article/hooks/useDeleteArticle'
+import { Article } from '../../features/admin/article/types/article'
+import { CustomizedSnackbar } from '../../components/admin/elements/CustomizedSnackbar'
+import { ConfirmModal } from '../../components/admin/elements/ConfirmModal'
 
 // 下書一覧画面
-export const AdminDraftIndexArticle: FC = () => {
+export const AdminDraftIndexArticle: React.FC = () => {
+  const fetchArticlesHooks = useFetchArticles()
+  const deleteArticleHooks = useDeleteArticle()
+  const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false)
+  const [selectedArticle, setSelectedArticle] = useState<Article>()
+  const [isOpenSnackbar, setIsOpenSnackbar] = useState(false)
+
+  // 初回遷移時に表示するデータを取得する
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      await fetchArticlesHooks.fetchArticles(true)
+    }
+    fetchInitialData()
+  }, [])
+
+  const executeDeleteArticle = async () => {
+    console.log('delete')
+    setIsOpenConfirmModal(false)
+  }
+
   return (
     <>
+      <CustomizedSnackbar
+        isOpen={isOpenSnackbar}
+        handleClose={() => {
+          setIsOpenSnackbar(false)
+        }}
+        message="Success!"
+      />
+      <ConfirmModal
+        isOpen={isOpenConfirmModal}
+        title="削除確認"
+        description="記事を1件削除します。よろしいですか？"
+        handleClose={() => {
+          setIsOpenConfirmModal(false)
+        }}
+        handleSubmit={() => {
+          executeDeleteArticle()
+        }}
+      />
       <AdminTemplate title="管理画面">
         <Grid container spacing={3}>
           <ArticleTable
--- frontend/src/pages/admin/AdminHome.tsx
+++ frontend/src/pages/admin/AdminHome.tsx
@@ -1,13 +1,33 @@
-import { FC } from 'react'
+import React, { useState, useEffect } from 'react'
 import { Grid, Paper, Button } from '@mui/material'
 import { AdminTemplate } from '../../components/admin/templates/AdminTemplate'
 import { Chart } from '../../features/admin/chart/components/Chart'
 import { AccessCountBox } from '../../features/admin/access_counts/components/AccessCountBox'
 import { ArticleTable } from '../../features/admin/article/components/ArticleTable'
+import { useFetchArticles } from '../../features/admin/article/hooks/useFetchArticles'
+import { useDeleteArticle } from '../../features/admin/article/hooks/useDeleteArticle'
+import { Article } from '../../features/admin/article/types/article'
+import { CustomizedSnackbar } from '../../components/admin/elements/CustomizedSnackbar'
+import { ConfirmModal } from '../../components/admin/elements/ConfirmModal'
+
 
 // NOTE: アクセス数の他に、記事の投稿数、タグごとの記事の本数のグラフがあってもいいかも
 
-export const AdminHome: FC = () => {
+export const AdminHome: React.FC = () => {
+  const fetchArticlesHooks = useFetchArticles()
+  const deleteArticleHooks = useDeleteArticle()
+  const [isOpenConfirmModal, setIsOpenConfirmModal] = useState(false)
+  const [selectedArticle, setSelectedArticle] = useState<Article>()
+  const [isOpenSnackbar, setIsOpenSnackbar] = useState(false)
+
+  // 初回遷移時に表示するデータを取得する
+  useEffect(() => {
+    const fetchInitialData = async () => {
+      await fetchArticlesHooks.fetchArticles(false)
+    }
+    fetchInitialData()
+  }, [])
+
   const accessCount = {
     totalAccessCount: 3024,
     updatedAt: '2023/7/1',
@@ -29,8 +49,31 @@ export const AdminHome: FC = () => {
     createChartData('2023/7/9', undefined),
   ]
 
+  const executeDeleteArticle = async () => {
+    console.log('delete')
+    setIsOpenConfirmModal(false)
+  }
+
   return (
     <>
+      <CustomizedSnackbar
+        isOpen={isOpenSnackbar}
+        handleClose={() => {
+          setIsOpenSnackbar(false)
+        }}
+        message="Success!"
+      />
+      <ConfirmModal
+        isOpen={isOpenConfirmModal}
+        title="削除確認"
+        description="記事を1件削除します。よろしいですか？"
+        handleClose={() => {
+          setIsOpenConfirmModal(false)
+        }}
+        handleSubmit={() => {
+          executeDeleteArticle()
+        }}
+      />
       <AdminTemplate title="管理画面">
         <Grid container spacing={3}>
           {/* Chart */}
@@ -59,7 +102,10 @@ export const AdminHome: FC = () => {
               <AccessCountBox accessCount={accessCount} />
             </Paper>
           </Grid>
-          <ArticleTable title="投稿済記事一覧" isDraft={false} />
+          <ArticleTable
+            title="投稿済記事一覧"
+            isDraft={false}
+          />
         </Grid>
       </AdminTemplate>
     </>
